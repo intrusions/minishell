e2480a546988aeea2b0ca31d2896ee7a72f0c62c
intrusions <xeloronkali@gmail.com>
2022-07-19 15:25:00 +0200

    update impossible cmd

--- srcs/parsing/impossible_cmd.c
+++ srcs/parsing/impossible_cmd.c
@@ -6,7 +6,7 @@
 /*   By: xel <xel@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 23:06:40 by jucheval          #+#    #+#             */
-/*   Updated: 2022/07/19 12:05:17 by xel              ###   ########.fr       */
+/*   Updated: 2022/07/19 15:23:57 by xel              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,23 +40,113 @@ int	check_multiple_pipe(char *str)
 	return (1);
 }
 
-// int	check_redir_before_pipe(char *str)
-// {
-// 	int	i;
+int	pipe_with_redir_after(char *str)
+{
+	int	i;
+	int	quote;
 
-// 	i = 0;
-// 	while (str[i])
-// 	{
-// 		while (str[i] && str[i] != '|')
-// 			i++;
-// 		if (str[i] == '|' && (str[i - 1] == '<' && (i > 0)))
-// 			return (printf("syntax error near unexpected token \'|\'\n"), 0);
-// 		else if ((str[i] == '|' && (str[i - 1] == '>' && (i > 0))))
-// 			return (printf("syntax error near unexpected token \'newline\'\n"), 0);
-// 		i++;
-// 	}
-// 	return (1);
-// }
+	i = 0;
+	quote = 0;
+	while (str[i])
+	{
+		quote = what_state(str, i);
+		if (str[i] == '|' && !quote)
+		{
+			i++;
+			if (str[i] == ' ')
+				i++;
+			if ((str[i] == '<') || (str[i] == '>'))
+				return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	pipe_with_redir_before(char *str)
+{
+	int	i;
+	int	quote;
+
+	i = 0;
+	quote = 0;
+	while (str[i])
+	{
+		quote = what_state(str, i);
+		if (str[i] == '|' && !quote)
+		{
+			i--;
+			if (str[i] == ' ')
+				i--;
+			if ((str[i] == '<') || (str[i] == '>'))
+				return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	multiple_redir(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '>' || str[i] == '<')
+		{
+			i++;
+			if (str[i] == ' ')
+				i++;
+			if (str[i] == '<' || str[i] == '>')
+			{
+				i++;
+				if (str[i] == ' ')
+					i++;
+				if (str[i] == '<' || str[i] == '>')
+					return (0);
+			}
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	redir_oppose(char *str)
+{
+	int	i;
+	int	quote;
+
+	i = 0;
+	while (str[i])
+	{
+		quote = what_state(str, i);
+		if (str[i] == '>' && !quote)
+		{
+			i++;
+			if (str[i] == ' ')
+				i++;
+			if (str[i] == '<')
+				return (0);
+		}
+		i++;
+	}
+	i = 0;
+	while (str[i])
+	{
+		quote = what_state(str, i);
+		if (str[i] == '<' && !quote)
+		{
+			i++;
+			if (str[i] == ' ')
+				i++;
+			if (str[i] == '>')
+				return (0);
+		}
+		i++;
+	}
+	return (1);
+}
 
 int	check_impossible_cmd(t_cmd *cmd)
 {
@@ -64,5 +154,14 @@ int	check_impossible_cmd(t_cmd *cmd)
 	if (!check_multiple_pipe(cmd->initial_cmd))
 		return (0);
 	replace_negativ_char(cmd->initial_cmd);
+
+	if (!multiple_redir(cmd->initial_cmd))
+		return (printf("cest multiple redir\n"), 0);
+	if (!pipe_with_redir_after(cmd->initial_cmd))
+		return (printf("cest after\n"), 0);
+	if (!pipe_with_redir_before(cmd->initial_cmd))
+		return (printf("cest before\n"), 0);
+	if (!redir_oppose(cmd->initial_cmd))
+		return (printf("cest opposer\n"), 0);
 	return (1);
 }
